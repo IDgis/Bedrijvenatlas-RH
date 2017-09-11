95e24df535f85c2aee994ddc692e9cca06f738a4
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-09-11 10:40:43 +0200

    Added extra question to the wizard page

--- imports/ui/pages/wizard/WizardPage.jsx
+++ imports/ui/pages/wizard/WizardPage.jsx
@@ -11,7 +11,6 @@ const styles = {
         marginBottom: 16
     },
     questions: {
-        position: 'absolute',
         textAlign: 'left',
         left: '35%',
         top: '5%',
@@ -52,37 +51,41 @@ export default class WizardPage extends Component {
         return (
             <div>
                 <div style={styles.questions}>
-                    <div>
-                        <h2 style={styles.h2}>Naar wat voor pand bent u op zoek?</h2>
-                        <RadioButtonGroup name="pand" defaultSelected="beide" onChange={this.setPandValue.bind(this)}>
-                            <RadioButton value="bestaand" label="Ik zoek een bestaand pand" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="nieuwbouw" label="Ik wil nieuw bouwen" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                        </RadioButtonGroup>
+                    <div style={{position:'absolute', left:'15%', top:'20%'}} >
+                        <div>
+                            <h2 style={styles.h2}>Naar wat voor pand bent u op zoek?</h2>
+                            <RadioButtonGroup name="pand" defaultSelected="beide" onChange={this.setPandValue.bind(this)}>
+                                <RadioButton value="bestaand" label="Ik zoek een bestaand pand" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="nieuwbouw" label="Ik wil nieuw bouwen" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                            </RadioButtonGroup>
+                        </div><br/><br/>
+                        <div>
+                            <h2 style={styles.h2}>Wilt u kopen of huren?</h2>
+                            <RadioButtonGroup name="huur-koop" defaultSelected="beide" onChange={this.setHuurKoopValue.bind(this)}>
+                                <RadioButton value="koop" label="Kopen" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="huur" label="Huren" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                            </RadioButtonGroup>
+                        </div>
                     </div>
-                    <div>
-                        <h2 style={styles.h2}>Wilt u kopen of huren?</h2>
-                        <RadioButtonGroup name="huur-koop" defaultSelected="beide" onChange={this.setHuurKoopValue.bind(this)}>
-                            <RadioButton value="koop" label="Kopen" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="huur" label="Huren" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                        </RadioButtonGroup>
-                    </div>
-                    <div>
-                        <h2 style={styles.h2}>Bent u op zoek naar een kantoor of bedrijfshal?</h2>
-                        <RadioButtonGroup name="pandtype" defaultSelected="beide" onChange={this.setHuurKoopValue.bind(this)}>
-                            <RadioButton value="kantoor" label="Kantoor" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="hal" label="Bedrijfshal" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                        </RadioButtonGroup>
-                    </div>
-                    <div>
-                        <h2 style={styles.h2}>In welke plaats zoekt u de mogelijkheden?</h2>
-                        <RadioButtonGroup name="plaats" defaultSelected="beide" onChange={this.setPlaatsValue.bind(this)}>
-                            <RadioButton value="rijssen" label="Rijssen" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="holten" label="Holten" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                            <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill: '#730049'}} labelStyle={{color:'#333'}} />
-                        </RadioButtonGroup>
+                    <div style={{position:'absolute', right:'15%', top:'20%'}} >
+                        <div>
+                            <h2 style={styles.h2}>Bent u op zoek naar een kantoor of bedrijfshal?</h2>
+                            <RadioButtonGroup name="pandtype" defaultSelected="beide" onChange={this.setHuurKoopValue.bind(this)}>
+                                <RadioButton value="kantoor" label="Kantoor" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="hal" label="Bedrijfshal" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                            </RadioButtonGroup>
+                        </div><br/><br/>
+                        <div>
+                            <h2 style={styles.h2}>In welke plaats zoekt u de mogelijkheden?</h2>
+                            <RadioButtonGroup name="plaats" defaultSelected="beide" onChange={this.setPlaatsValue.bind(this)}>
+                                <RadioButton value="rijssen" label="Rijssen" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="holten" label="Holten" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                                <RadioButton value="beide" label="Geen voorkeur" style={styles.radioButton} iconStyle={{fill:Meteor.settings.public.colorGemeente}} labelStyle={{color:'#333'}} />
+                            </RadioButtonGroup>
+                        </div>
                     </div>
                 </div>
                 <RaisedButton 
@@ -90,7 +93,7 @@ export default class WizardPage extends Component {
                     label="Naar de viewer" 
                     labelPosition="before"
                     primary={true} 
-                    buttonStyle={{backgroundColor: '#730049'}} 
+                    buttonStyle={{backgroundColor:Meteor.settings.public.colorGemeente}} 
                     style={styles.button} 
                     icon={ <img style={{height:'100%'}} src="/images/navigate_next_w.png" /> }
                     onTouchTap={this.setLayers.bind(this)}
