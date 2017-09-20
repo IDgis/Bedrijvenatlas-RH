765fbecfd038189cea3769753b32ebf650a6b78f
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-09-20 17:35:49 +0200

    Changed link to button

--- imports/ui/pages/viewer/components/popups/Popup.jsx
+++ imports/ui/pages/viewer/components/popups/Popup.jsx
@@ -17,10 +17,10 @@ export default class Popup extends Component {
         let searchFields = this.props.searchFields;
         let returnField = [];
         let link = (this.props.title === Meteor.settings.public.laagNaam.ibis && this.props.selectedFeature.get('BEDR_TERR').startsWith('Vletgaarsmaten'))
-            ? <div><br /><a href='https://ondernemersloket.rijssen-holten.nl/home/publicatie/vletgaarsmaten-holten' target='_blank'><b>Gemeente Rijssen-Holten</b></a><br /></div>
+            ? <div><br /><RaisedButton href={'https://ondernemersloket.rijssen-holten.nl/home/publicatie/vletgaarsmaten-holten'} target='_blank' label='Gemeente Rijssen-Holten' /><br /></div>
             : <div></div>;
         let fundalink = (this.props.title === Meteor.settings.public.laagNaam.teKoop || this.props.title === Meteor.settings.public.laagNaam.teHuur)
-            ? <div><br /><a href={this.props.selectedFeature.get('URL')} target='_blank'><b>Naar Funda</b></a><br /></div>
+            ? <div><br /><RaisedButton href={this.props.selectedFeature.get('URL')} target='_blank' label='Funda' /><br /></div>
             : <div></div>
         let streetviewbutton = (this.props.title === Meteor.settings.public.laagNaam.teKoop || this.props.title === Meteor.settings.public.laagNaam.teHuur)
             ? <div><br /><RaisedButton label='Streetview' onClick={this.props.openStreetView} /><br /></div>
@@ -40,7 +40,7 @@ export default class Popup extends Component {
                     <div style={{position:'relative', left:'20px'}}><br />
                         <h3><u>{this.props.title}</u></h3>
                         {returnField} <br />
-                        <a href={ruimtelijkePlannenUrl} target='_blank'><b>Bekijk bestemmingsplan</b></a>
+                        <RaisedButton href={ruimtelijkePlannenUrl} target='_blank' label='Bestemmingsplan' /><br />
                         {link}
                         {fundalink}
                         {streetviewbutton}
