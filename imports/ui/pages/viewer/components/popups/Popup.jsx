f35ac321b892c347f8a6f88ee65ead1455693bc2
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-09-26 15:39:37 +0200

    Added streetview to layer kvk bedrijven

--- imports/ui/pages/viewer/components/popups/Popup.jsx
+++ imports/ui/pages/viewer/components/popups/Popup.jsx
@@ -46,28 +46,30 @@ export default class Popup extends Component {
      * The main render method that will render the component to the screen
      */
     render() {
+        let laagNaam = Meteor.settings.public.laagNaam;
+        let title = this.props.title;
+        let searchFields = this.props.searchFields;
 
-        let link = (this.props.title === Meteor.settings.public.laagNaam.ibis && this.props.selectedFeature.get('BEDR_TERR').startsWith('Vletgaarsmaten'))
+        let link = (title === laagNaam.ibis && this.props.selectedFeature.get('BEDR_TERR').startsWith('Vletgaarsmaten'))
             ? <div><br /><RaisedButton href={'https://ondernemersloket.rijssen-holten.nl/home/publicatie/vletgaarsmaten-holten'} target='_blank' label='Gemeente Rijssen-Holten' /><br /></div>
             : <div></div>;
-        let fundalink = (this.props.title === Meteor.settings.public.laagNaam.teKoop || this.props.title === Meteor.settings.public.laagNaam.teHuur)
+        let fundalink = (title === laagNaam.teKoop || title === laagNaam.teHuur)
             ? <div><br /><RaisedButton href={this.props.selectedFeature.get('URL')} target='_blank' label='Funda' /><br /></div>
             : <div></div>
-        let streetviewbutton = (this.props.title === Meteor.settings.public.laagNaam.teKoop || this.props.title === Meteor.settings.public.laagNaam.teHuur)
+        let streetviewbutton = (title === laagNaam.teKoop || title === laagNaam.teHuur || title === laagNaam.kvk)
             ? <div><br /><RaisedButton label='Streetview' onClick={this.props.openStreetView} /><br /></div>
             : <div></div>;
         let location = this.props.map.getView().calculateExtent(this.props.map.getSize());
         let ruimtelijkePlannenUrl = 'http://www.ruimtelijkeplannen.nl/web-roo/roo/bestemmingsplannen?' +
             'bbx1=' + location[0] + '&bby1=' + location[1] + '&bbx2=' + location[2] + '&bby2=' + location[3];
 
-        let searchFields = this.props.searchFields;
         let returnField = [];
         for(let i in searchFields) {
             let result = <div key={i}><b>{searchFields[i]}:</b> {this.props.selectedFeature.get(searchFields[i])}<br /></div>;
             returnField.push(result);
         }
 
-        let categorieInfo = <div><b>CATEGORIE:</b> {this.getCategorieInfo()}</div>;
+        //let categorieInfo = <div><b>CATEGORIE:</b> {this.getCategorieInfo()}</div>;
 
         return(
             <div style={{position: 'absolute', top: (this.props.coords.y - 100), left: this.props.coords.x}}>
@@ -75,7 +77,6 @@ export default class Popup extends Component {
                     <div style={{position:'relative', left:'20px'}}><br />
                         <h3><u>{this.props.title}</u></h3>
                         {returnField} <br />
-                        {categorieInfo} <br />
                         <RaisedButton href={ruimtelijkePlannenUrl} target='_blank' label='Bestemmingsplan' /><br />
                         {link}
                         {fundalink}
