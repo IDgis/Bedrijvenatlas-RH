3f2efd440f2c675617ab3ccc6e2a706b6ef64292
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2017-10-03 13:18:08 +0200

    Added dockerfile

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import '../imports/startup/client';
+import { Meteor } from 'meteor/meteor';
--- imports/ui/pages/welcome/WelcomePage.jsx
+++ imports/ui/pages/welcome/WelcomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import Paper from 'material-ui/Paper';
 
 import ViewerLink from './ViewerLink';
