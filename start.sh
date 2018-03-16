f0afaaa5a508a05fda7ef6ba1be73ac708f4deec
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-03-16 13:46:11 +0100

    raised image version

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo deploying bedrijvenatlas...
 
 export METEOR_SETTINGS="$(cat ./settings.json)"
 export ROOT_URL=http://192.168.99.100
-export VERSION=1.0.2
+export VERSION=1.0.4
 
 docker-compose \
 	-p rh \
