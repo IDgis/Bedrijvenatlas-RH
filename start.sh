07b382c5f97f7ee8377f04995885814efe8b5037
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-08-23 15:43:16 +0200

    adjusted installation

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@
 
 echo deploying bedrijvenatlas...
 
-export METEOR_SETTINGS="$(cat ./settings.json)"
+export METEOR_SETTINGS="$(cat ./settings-rh.json)"
 export ROOT_URL=http://192.168.99.100
-export VERSION=1.0.4
+export VERSION=1.0.5
+export GEMEENTE=gemeente-rijssen-holten
 
 docker-compose \
 	-p rh \
