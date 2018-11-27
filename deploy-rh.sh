9121e8e1e4ee914478de021d2b59cfd147bd94e3
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-11-27 16:23:57 +0100

    move searchbar css inline to component

--- deploy-rh.sh
+++ deploy-rh.sh
@@ -5,7 +5,7 @@ echo deploying bedrijvenatlas Rijssen-Holten...
 export METEOR_SETTINGS="$(cat ./settings-rh.json)"
 export ROOT_URL=http://192.168.99.100
 export IMAGE_GEMEENTE=rh
-export VERSION=1.0.5
+export VERSION=1.1.1
 export GEMEENTE=gemeente-rijssen-holten
 
 docker-compose \
