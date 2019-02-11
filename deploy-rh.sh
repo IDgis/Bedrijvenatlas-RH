b76669dcbd738b5b0b670d13ac2c1d015253e04e
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2019-02-11 09:01:05 +0100

    add compose tls version for newer docker machine

--- deploy-rh.sh
+++ deploy-rh.sh
@@ -11,6 +11,8 @@ export BEDRIJVENTERREINEN_FEATURE_BOUNDS="https://rijssen-holten-pub.geopublishe
 
 export FUNDA_SCRAPER_VERSION=1.1.3
 
+export COMPOSE_TLS_VERSION=TLSv1_2
+
 docker-compose \
 	-p rh \
 	up -d
