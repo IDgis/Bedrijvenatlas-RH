3147a1563387ea72adaa8c8bf69a19f3a9b23b45
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2019-02-14 09:05:34 +0100

    refactored ContactLink

--- src/components/link/ContactLink.js
+++ src/components/link/ContactLink.js
@@ -4,26 +4,28 @@ const linkStyle = {
     textDecoration: 'none',
     color: '#000',
     opacity: 1
-}
+};
 
 const iconStyle = {
     position: 'relative',
     width: '80%',
     boxShadow: '3px 3px 10px 2px grey'
-}
+};
 
 const textStyle = {
     position: 'relative',
     color: '#000'
-}
+};
 
-export default ContactLink = () => (
+const ContactLink = ({settings}) => (
     <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-        <a href={ Meteor.settings.public.gemeenteConfig.contactPage } target="_blank" style={linkStyle}>
-            <img style={iconStyle} src={ Meteor.settings.public.gemeenteConfig.logoContactKnop } />
+        <a href={ settings.gemeenteConfig.contactPage } target="_blank" style={linkStyle}>
+            <img style={iconStyle} src={ settings.gemeenteConfig.logoContactKnop } alt="" />
             <div style={textStyle}>
                 <h2>Meer informatie</h2>
             </div>
         </a>
     </div>
 );
+
+export default ContactLink;
