9cf53e61544399c0fc39b642e2c84d78920756d5
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2019-02-14 09:04:48 +0100

    refactored WelcomePage

---
+++ src/components/page/Welcome.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ContactLink from '../link/ContactLink';
+import ViewerLink from '../link/ViewerLink';
+import WizardLink from '../link/WizardLink';
+
+const Welcome = ({settings}) => {
+    sessionStorage.setItem('pand', '');
+    sessionStorage.setItem('huur-koop', '');
+    sessionStorage.setItem('plaats', '');
+
+    return (
+        <div>
+            <h1>Vind hier de ideale locatie voor uw bedrijf!</h1>
+            <div className="row" style={{marginTop:'2em'}}>
+                <WizardLink settings={settings} />
+                <ViewerLink settings={settings} />
+                <ContactLink settings={settings} />
+            </div>
+        </div>
+    );
+}
+
+export default Welcome;
